03f2f489c65296aab6eb17e0b016ab5110f0bee3
NaiTing Liu <sorarize@gmail.com>
2014-11-25 08:20:42 +0900

    Demo released

---
+++ javascripts/app.js
@@ -0,0 +1,98 @@
+(function() {
+  var $artist, $body, $content, $externalLink, $object, $objectTitle, $random, $togglers, DEBUG, counter, getItems, setBackground, showItem, showItems, xx;
+
+  xx = function(t) {
+    return DEBUG && console.log(t);
+  };
+
+  DEBUG = true;
+
+  $body = $('body');
+
+  $content = $('.content');
+
+  counter = null;
+
+  $body.on('click', 'a[href=#]', function(event) {
+    return event.preventDefault();
+  });
+
+  setBackground = function(url) {
+    return $content.css('background-image', "url('/uploads/" + url + "')");
+  };
+
+  getItems = function(artist, length) {
+    var res;
+
+    res = _.where(data, {
+      artist: artist
+    });
+    if (length != null) {
+      res = _.sample(res, length);
+    }
+    return res;
+  };
+
+  showItems = function(artist) {
+    var duration, index, items, _item, _next, _show;
+
+    items = getItems(artist, 3);
+    index = 0;
+    duration = 3000;
+    _item = function() {
+      return items[index];
+    };
+    _next = function() {
+      index++;
+      if (index === items.length) {
+        index = 0;
+      }
+      return _show();
+    };
+    _show = function() {
+      if (counter != null) {
+        clearTimeout(counter);
+      }
+      showItem(_item());
+      return counter = setTimeout(_next, duration);
+    };
+    return _show();
+  };
+
+  showItem = function(item) {
+    setBackground(item.url);
+    $artist.text(item.artist_name);
+    $object.text(item.object.name);
+    return $objectTitle.text(item.object.title);
+  };
+
+  $random = $('.random');
+
+  if ($random.length) {
+    setBackground(_.sample(data).url);
+  }
+
+  $artist = $('.artist');
+
+  $object = $('.object');
+
+  $objectTitle = $('.object-title');
+
+  $externalLink = $('.external-link');
+
+  $togglers = $('[data-artist]');
+
+  $togglers.on('click', function() {
+    var $toggler, artist;
+
+    $toggler = $(this);
+    artist = $toggler.data('artist');
+    $togglers.removeClass('active');
+    $toggler.addClass('active');
+    showItems(artist);
+    return $externalLink.removeClass('hide');
+  });
+
+  $togglers.eq(0).trigger('click');
+
+}).call(this);
---
+++ javascripts/data.js
@@ -0,0 +1,89 @@
+(function() {
+  window.rawData = [
+    {
+      artist: 'Yao Teng Yuan',
+      artist_name: '姚',
+      object: {
+        name: '榆鈞',
+        title: '表演者'
+      },
+      items: ['500T-01007.jpg', '500T-02013.jpg', 'Negative0-21-21(1).jpg', 'Negative0-24-24(1).jpg', 'Negative0-29-29(1).jpg']
+    }, {
+      artist: 'Etang Chen',
+      artist_name: '陳',
+      object: {
+        name: 'ＺＯＥＹ',
+        title: '表演者'
+      },
+      items: ['000032410034.jpg', '000032470005.jpg', '000032490002.jpg', '000032510009.jpg']
+    }, {
+      artist: 'JaMie Lin',
+      artist_name: '林小嬉',
+      object: {
+        name: '育君',
+        title: '表演者'
+      },
+      items: ['Negative0-1.jpg', 'Negative0-2.jpg', 'Negative0-3.jpg', 'Negative0-7.jpg']
+    }, {
+      artist: 'Jan Kay',
+      artist_name: '詹凱',
+      object: {
+        name: '宛璇',
+        title: '表演者'
+      },
+      items: ['1.jpg', '2.jpg', '5.jpg', '6.jpg']
+    }, {
+      artist: 'Jersey Hsin-le Yen',
+      artist_name: '顏',
+      object: {
+        name: 'ａｍｙ',
+        title: '表演者'
+      },
+      items: ['Negative0-09-09(1).jpeg', 'Negative0-24-24(1).jpeg', 'Negative0-25-25(1).jpeg', 'Negative0-32-32(1).jpeg']
+    }, {
+      artist: 'Tai Ju Hsieh',
+      artist_name: '謝',
+      object: {
+        name: '孟書',
+        title: '表演者'
+      },
+      items: ['F1040006-1.jpg', 'F1040014-1.jpg', 'F1040021.jpg', 'F1050028-1.jpg']
+    }, {
+      artist: 'Tai Ju Hsieh',
+      artist_name: '謝',
+      object: {
+        name: '紅旗',
+        title: '表演者'
+      },
+      items: ['_MG_1295S.jpg', '_MG_1303S.jpg', '_MG_1351S.jpg', '_MG_1353S.jpg', '_MG_1365S.jpg']
+    }, {
+      artist: 'You Wei Chen',
+      artist_name: '陳',
+      object: {
+        name: '啊寡',
+        title: '表演者'
+      },
+      items: ['22680001.jpg', '22680006.jpg', '22680010.jpg', '22690001.jpg', '22710004.jpg', '22710011.jpg']
+    }, {
+      artist: 'You-Na Wang',
+      artist_name: '王',
+      object: {
+        name: '修和',
+        title: '表演者'
+      },
+      items: ['scan053.jpg', 'scan143拷貝.jpg', 'scan151.jpg', 'scan158.jpg']
+    }
+  ];
+
+  window.data = _.flatten(_.map(rawData, function(hash) {
+    return _.map(hash.items, function(filename) {
+      return {
+        url: filename,
+        object: hash.object,
+        artist: hash.artist,
+        artist_name: hash.artist_name
+      };
+    });
+  }));
+
+}).call(this);
